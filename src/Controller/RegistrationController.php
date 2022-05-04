ad0f8d2497e81b025807a1e0b620ae0047c82254
QuentinPham <qej.pham@gmail.com>
2022-05-04 10:13:00 +0200

    04/05/2022
    Suppression dependence rate limiter + correction mineure

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -11,9 +11,7 @@ use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\RateLimiter\RateLimiterFactory;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
@@ -26,73 +24,66 @@ class RegistrationController extends AbstractController
                              UserPasswordHasherInterface $userPasswordHasher,
                              EntityManagerInterface      $entityManager,
                              EleveRepository             $eleveRepository,
-                             AdulteRepository            $adulteRepository,
-                             RateLimiterFactory          $anonymousApiLimiter): Response
+                             AdulteRepository            $adulteRepository): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
-        $limiter = $anonymousApiLimiter->create($request->getClientIp());
 
-        // the argument of consume() is the number of tokens to consume
-        // and returns an object of type Limit
-        if (false === $limiter->consume(1)->isAccepted()) {
-            $error = throw new TooManyRequestsHttpException('dans une heure', 'Vous avez fait trop de demande d\'inscription !');
-        } else {
-            if ($form->isSubmitted() && $form->isValid()) {
-                $eleveFound = $eleveRepository->findByNomPrenomDateNaissance($form->get('nomUser')->getData(),
-                    $form->get('prenomUser')->getData(), $form->get('dateNaissanceUser')->getData());
+        if ($form->isSubmitted() && $form->isValid()) {
+            $eleveFound = $eleveRepository->findByNomPrenomDateNaissance($form->get('nomUser')->getData(),
+                $form->get('prenomUser')->getData(), $form->get('dateNaissanceUser')->getData());
 
-                $adulteFound = $adulteRepository->findByNomPrenomDateNaissance($form->get('nomUser')->getData(),
-                    $form->get('prenomUser')->getData(), $form->get('dateNaissanceUser')->getData());
+            $adulteFound = $adulteRepository->findByNomPrenomDateNaissance($form->get('nomUser')->getData(),
+                $form->get('prenomUser')->getData(), $form->get('dateNaissanceUser')->getData());
 
-                if ($eleveFound || $adulteFound) {
-                    // encode the plain password
-                    $user->setPassword(
-                        $userPasswordHasher->hashPassword(
-                            $user,
-                            $form->get('plainPassword')->getData()
-                        )
-                    );
-                    $user->setTokenHash(md5($user->getNomUser() . $user->getEmail()));
-                    $user->setIsVerified(true);
+            if ($eleveFound || $adulteFound) {
+                // encode the plain password
+                $user->setPassword(
+                    $userPasswordHasher->hashPassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    )
+                );
+                $user->setTokenHash(md5($user->getNomUser() . $user->getEmail()));
+                $user->setIsVerified(true);
 
-                    if ($eleveFound) {
-                        $user->setRoles([User::ROLE_ELEVE]);
-                    } elseif ($adulteFound) {
-                        $user->setRoles([User::ROLE_ADULTES]);
-                    } else {
-                        $user->setRoles([User::ROLE_USER]);
-                    }
-
-                    $entityManager->persist($user);
-                    $entityManager->flush();
-
-                    if ($eleveFound) {
-                        $eleveFound->setCompteEleve($user);
-                    } elseif ($adulteFound) {
-                        $adulteFound->setCompteAdulte($user);
-                    }
-
-                    $entityManager->flush();
-
-                    $this->addFlash(
-                        'successInscription',
-                        'Votre inscription a été validée. Vous pouvez vous connecter.'
-                    );
-
-                    return $this->redirectToRoute('app_login');
+                if ($eleveFound) {
+                    $user->setRoles([User::ROLE_ELEVE]);
+                } elseif ($adulteFound) {
+                    $user->setRoles([User::ROLE_ADULTES]);
                 } else {
-                    $this->addFlash(
-                        'failedInscription',
-                        'Votre demande d\'inscription a été refusée.
-                    Vous n\'êtes pas reconnu en tant qu\'élève ou personnel de l\'établissement.
-                    Merci de vérifier que vos données
-                     (nom, prénom et date de naissance) correspondent à celles données à l\'administration.'
-                    );
+                    $user->setRoles([User::ROLE_USER]);
                 }
+
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                if ($eleveFound) {
+                    $eleveFound->setCompteEleve($user);
+                } elseif ($adulteFound) {
+                    $adulteFound->setCompteAdulte($user);
+                }
+
+                $entityManager->flush();
+
+                $this->addFlash(
+                    'successInscription',
+                    'Votre inscription a été validée. Vous pouvez vous connecter.'
+                );
+
+                return $this->redirectToRoute('app_login');
+            } else {
+                $this->addFlash(
+                    'failedInscription',
+                    'Votre demande d\'inscription a été refusée.
+                Vous n\'êtes pas reconnu en tant qu\'élève ou personnel de l\'établissement.
+                Merci de vérifier que vos données
+                 (nom, prénom et date de naissance) correspondent à celles données à l\'administration.'
+                );
             }
         }
+
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
